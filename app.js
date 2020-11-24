fefa1edadb763e96168957289bd77443084b751d
iamy8000 <amy8000@gmail.com>
2020-11-25 03:19:46 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
-const session = require('express-session')
-const usePassport = require('./config/passport')
-const passport = require('passport')
 const flash = require('connect-flash')
-const { authenticator } = require('./middleware/auth')
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
+const routes = require('./routes')
+const usePassport = require('./config/passport')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -37,149 +31,13 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
-  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
+app.use(routes)
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  const UserId = req.user.id
-  return Todo.create({ name, UserId })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  const errors = []
-
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
-  }
-  if (password !== confirmPassword) {
-    errors.push({ message: '密碼與確認密碼不相符！' })
-  }
-  if (errors.length) {
-    return res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      confirmPassword
-    })
-  }
-
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      errors.push({ message: '這個 Email 已經註冊過了。' })
-      return res.render('register', {
-        errors,
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash,
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-
-})
-
-app.get('/users/logout', (req, res) => {
-  req.logout()
-  req.flash('success_msg', '你已經成功登出。')
-  res.redirect('/users/login')
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const UserId = req.user.id
-  const id = req.params.id
-
-  return Todo.findOne({ where: { id, UserId } })
-    .then(todo => res.render('edit', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const UserId = req.user.id
-  const id = req.params.id
-  const { name, isDone } = req.body
-
-  return Todo.findOne({ where: { id, UserId } })
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const UserId = req.user.id
-  const id = req.params.id
-
-  return Todo.findOne({ where: { id, UserId } })
-    .then(todo => todo.destroy())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/', authenticator, (req, res) => {
-  const UserId = req.user.id
-  console.log(UserId)
-  return Todo.findAll({
-    where: { UserId },
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
-})
-
-app.get('/auth/facebook', passport.authenticate('facebook', {
-  scope: ['email', 'public_profile']
-}))
-
-app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/',
-  failureRedirect: 'users/login'
-}))
-
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
---
+++ routes/modules/home.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
+
+router.get('/', (req, res) => {
+  const UserId = req.user.id
+  console.log(UserId)
+  return Todo.findAll({
+    where: { UserId },
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+module.exports = router
